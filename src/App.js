90021c26fedd4963a8d68db54f76d4c4d9bf48c0
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-19 13:53:17 +0100

    cleaning up npm files, setting up firebase,, displaying msgs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["Hello", "Hi", "Wag1"]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    //all logic to send msg goes here
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notssenger</h1>
+      <input
+        onClick={sendMessage}
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+
+      <button onClick={sendMessage}>Send Message</button>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
