9899f4f6bcc73ea007552d0f8741d59a28a411af
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-19 19:24:50 +0100

    differntiating between users + message styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import Message from "./Message";
 
 function App() {
+  //use state is var in react
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["Hello", "Hi", "Wag1"]);
+  const [messages, setMessages] = useState([
+    { username: "Elvo", text: "Wag1 bro" },
+    { username: "Billy", text: "Huurrrrr" },
+  ]);
+  const [username, setUsername] = useState("");
 
-  console.log(input);
-  console.log(messages);
+  //block of code executed based on condition
+  useEffect(() => {
+    //run code here
+    //const username= prompt("Please enter your username")
+    setUsername(prompt("Please enter your name"));
+  }, []); //condition if left blank this will run code once when app component loads
 
   const sendMessage = (event) => {
     //prevents from refreshing
     event.preventDefault();
     //all logic to send msg goes here
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
       <h1>Notssenger</h1>
-
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
@@ -42,7 +52,7 @@ function App() {
       </form>
 
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message username={username} message={message} />
       ))}
     </div>
   );
